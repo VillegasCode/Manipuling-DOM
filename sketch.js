832ffc518d792b3e5fded747a17ff11ee6f5bc1a
Carlos Villegas <carlos.villegasch@gmail.com>
2022-09-11 21:41:14 -0500

    Update position from H1 label and paragraph

--- sketch.js
+++ sketch.js
@@ -22,7 +22,7 @@ function changeColor() {
 function mousePressed() {
     h1.html("Now I will show you my favorite number")
     paragraph = createP("My favorite number is " + round(random(0,10)));
-    paragraph.position(500,-100+y);
+    paragraph.position(500,-50+y);
     y = y + 50;
 }
 
@@ -30,7 +30,7 @@ function draw() {
     clear();
     background(bgColor);
     fill(255,0,0);
-    h1.position(x,300);
+    h1.position(300,0);
     rect(x, 100, 50, 50);
     x = x + random(-3, 3);
     button.position(100,300);
